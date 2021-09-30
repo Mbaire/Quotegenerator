af8cd67f3d890981442e202a29eb6a0c7d005a61
mbaire <mbairemunge@gmail.com>
2021-10-01 01:27:03 +0300

    Created the navigation bar

---
+++ quote/src/app/quote-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quote-app-root',
+  template: './quote-app.component.html',
+
+})
+export class quoteAppComponent {
+  title = 'quote';
+}
