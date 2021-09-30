aa9ebff42249a32129b54b2f73ea652fe89b5612
mbaire <mbairemunge@gmail.com>
2021-10-01 01:07:31 +0300

    Modified ts file

--- quote/src/app/app.module.ts
+++ quote/src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { QuoteAppComponent } from './quote-app.component';
+import { CommentsComponent } from './comments/comments.component';
+import { NavComponent } from './nav/nav.component';
+import { AddQuoteComponent } from './add-quote/add-quote.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule }  from '@angular/material/icon'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    QuoteAppComponent,
+    CommentsComponent,
+    NavComponent,
+    AddQuoteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    QuoteAppComponent,
+    CommentsComponent,
+    NavComponent,
+    AddQuoteComponent,
+   
+  ],
 })
-export class AppModule { }
+export class AppModule {}
+
+  
+  
+
